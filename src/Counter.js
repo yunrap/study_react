9b302f94828ffbc1fa379a0bc9893c4ed3d2434b
yunrap <dbsdpwl7@gmail.com>
2022-06-27 23:58:28 +0900

    값변화, setState에  prevState추가

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,10 @@
 import { Component } from 'react';
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    // state의 초기값 설정하기
-    this.state = {
-      number: 0,
-      fixedNumber: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
   render() {
     const { number, fixedNumber } = this.state; // state를 조회할때는 this.state로 조회한다
     return (
@@ -18,7 +14,10 @@ class Counter extends Component {
         <button
           //onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
           onClick={() => {
-            this.setState({ number: number + 1 }); //변하는값만 설정
+            this.setState((prevState) => {
+              return { number: prevState.number + 1 };
+            }); //변하는값만 설정
+            this.setState((prevState) => ({ number: prevState.number + 1 }));
           }}
         >
           +1
