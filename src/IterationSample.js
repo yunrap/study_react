d69ee418878531ca93a4bed5fd548f2c9103933d
yunrap <dbsdpwl7@gmail.com>
2022-07-03 23:15:49 +0900

    map, concat, filter사용 (상태값변경하기)

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,10 +1,48 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const IterationSample = () => {
-  const names = ['눈사람', '얼음', '눈', '바람'];
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
-  return <ul>{nameList}</ul>;
+  const [names, setNames] = useState([
+    { id: 1, text: '눈사람' },
+    { id: 2, text: '눈' },
+    { id: 3, text: '얼음' },
+    { id: 4, text: '바람' },
+  ]);
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5); //새로운항목을 추가할때 사용할 id
+
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1); //nextId값에 1을 더해준다
+    setNames(nextNames); // names값을 업데이트한다
+    setInputText(''); // inputText값을 비운다.
+  };
+
+  const onChange = (e) => {
+    setInputText(e.target.value);
+    console.log(inputText);
+  };
+
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+
+  const nameList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
+  return (
+    <>
+      <input value={inputText} onChange={onChange}></input>
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>
+    </>
+  );
 };
 
 export default IterationSample;
