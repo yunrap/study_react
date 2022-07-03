e91413b7d9d414759d5132344430efba5910a924
yunrap <dbsdpwl7@gmail.com>
2022-07-03 22:27:34 +0900

    ref 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import EventPractice from './EventPractice';
-import Say from './Say';
+import { Component } from 'react';
+import ScrollBox from './ScrollBox';
 
-const App = () => {
-  return <EventPractice></EventPractice>;
-};
-
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)}></ScrollBox>
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 export default App;
---
+++ src/ScrollBox.js
@@ -0,0 +1,38 @@
+import { Component } from 'react';
+
+class ScrollBox extends Component {
+  scrollToBottom = () => {
+    const { scrollHeight, clientHeight } = this.box;
+    // 비구조화할당 const scrollHeigt = this.box.scrollHeight 같다
+
+    this.box.scrollTop = scrollHeight - clientHeight;
+  };
+  render() {
+    const style = {
+      border: '1px solid black',
+      height: '300px',
+      width: '300px',
+      overflow: 'auto',
+      position: 'relative',
+    };
+
+    const innerStyle = {
+      width: '100%',
+      height: '650px',
+      background: 'linear-gradient(white, black)',
+    };
+
+    return (
+      <div
+        style={style}
+        ref={(ref) => {
+          this.box = ref;
+        }}
+      >
+        <div style={innerStyle}></div>
+      </div>
+    );
+  }
+}
+
+export default ScrollBox;
