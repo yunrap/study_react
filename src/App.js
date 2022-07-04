91ec2ae3ae7a51097de619344ba6d38625600ddc
yunrap <dbsdpwl7@gmail.com>
2022-07-04 23:27:25 +0900

    AXios 추가중 파일 404

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import { Component } from 'react';
-import SassComponent from './SassComponent';
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <SassComponent />
-      </div>
-    );
-  }
-}
+import { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos/1").then(response => {
+      setData(response.data);
+    });
+  };
+  return (
+    <>
+    <div>
+      <button onClick={onClick}> 불러오기</button>
+    </div>
+    {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/>
+    </>
+  );
+};
 export default App;
