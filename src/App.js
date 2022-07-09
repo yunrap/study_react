f4dadda8616061affb87e46254d85c3a900dedbe
yunrap <dbsdpwl7@gmail.com>
2022-07-09 19:06:38 +0900

    Commit before yarn eject

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import axios from 'axios';
 import NewsList from './Components/NewsList';
+import SassComponent from './Components/SassComponent';
 
 const App = () => {
   return (
     <div>
-      <NewsList></NewsList>
+      <SassComponent></SassComponent>
     </div>
   );
 };
